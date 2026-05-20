0892e1e70136f336f8eab4d7ee9d604643ba45c7
Bernardo <you@example.com>
2026-05-20 00:01:08 -0300

    comentarios

--- src/App.java
+++ src/App.java
@@ -4,32 +4,32 @@ public class App {
     public static void main(String[] args) throws Exception {
     Scanner entrada = new Scanner(System.in);
     int operacao;    
-    do{
+    do{   // do while funciona de forma que o codigo continue rodando enquanto certa condiçao nao for atendida
 
-        Funcoes.menu();
-        operacao = entrada.nextInt();
+        Funcoes.menu(); // puxa a funçao menu do objeto funçoes
+        operacao = entrada.nextInt();  // cria a variavel operacao para se utilizar o switch em casos diferentes
         switch (operacao) {
-            case 1:
+            case 1: // caso 1 adiciona algum produto e sua quantidade
                 System.out.println("digite qual produto você quer adicionar e a quantidade: ");
                 break;
-            case 2:
+            case 2: // caso 2 mostra a lista de produtos 
                 System.out.println("lista de produtos");
                 break;
-            case 3:
+            case 3: // caso 3 atualiza os produtos existentes
                 System.out.println("qual produto voce quer atualizar");
                 break;
-            case 4:
+            case 4: // caso 4 exclui o produto selecionado
                 System.out.println("Selecione o produto que deseja excluir");
                 break;
-            case 0:
+            case 0: // caso 0 encerra o sistema
                 System.out.println("Parando funcionamento");
                 break;
-            default:
+            default: // default é usado para quando nenhum dos casos sao selecionados
                 System.out.println("Opção inválida! Tente novamente.");
                 break;
             }
 
-    }while (operacao != 0);
+    }while (operacao != 0); // enquanto nao for selecionado o caso 0 o codigo continuara rodando sempre que um caso é encerrado
         entrada.close();  
         System.out.println("sistema encerrado");
     }
--- src/Funcoes.java
+++ src/Funcoes.java
@@ -1,10 +1,10 @@
 public class Funcoes {
     
-    public static void menu(){
+    public static void menu(){ // funçao criada para chamar o menu sempre que se precisar sem ficar repetindo linhas e linhas de codigo
        
         String[] menuItens = {
-            "--- MENU DE OPERAÇÕES ---",
-            "1: Cadastrar produtos",
+            "--- MENU DE OPERAÇÕES ---",                //tranforma todo o menu em linhas para poder ser centralizado no terminal
+            "1: Cadastrar produtos",                    
             "2: Listar produtos",
             "3: Atualizar produto",
             "4: Excluir produto",
@@ -15,16 +15,13 @@ public class Funcoes {
         // Largura padrão do terminal
         int larguraTerminal = 80;
 
-        for (String linha : menuItens) {
+        for (String linha : menuItens) {                        
             // Calcula os espaços necessários para centralizar
-            int espacos = (larguraTerminal - linha.length()) / 2;
-            String formato = "%" + (espacos + linha.length()) + "s%n";
-            System.out.printf(formato, linha);
+            int espacos = (larguraTerminal - linha.length()) / 2;   //Calcula quantos espaços em branco são necessários para centralizar o texto   
+            String formato = "%" + (espacos + linha.length()) + "s%n";   //Cria uma formataçao do texto centralizado              
+            System.out.printf(formato, linha);  //imprime o menu de forma centralizada
         }
         
     }
-    
-
-
     
 }
